121737237edbaff832a2cd0609ffa82798528e0c
Tyler Hodzen <38892841+thodzen@users.noreply.github.com>
2020-08-06 12:40:52 -0400

    before lunch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms'; // This was added by me; Opt-in; NgModel lives here
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule // this was added by me and comes from the FormsModule import at the top of file
   ],
   providers: [],
   bootstrap: [AppComponent]
